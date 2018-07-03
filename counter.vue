7c96c6c850ad5c37ce217d74eaf92c324b3bd67f
蔡荣庚 <89987112@qq.com>
2018-07-03 17:10:08 +0800

    update

---
+++ counter.vue
@@ -0,0 +1,75 @@
+<style lang="scss" scoped>
+    .counter-component{
+        border: 1rpx solid #eee;
+        border-radius: 4rpx;
+        display: flex;
+        align-items: center;
+        align-content: center;
+        line-height: 1.5em;
+
+        .sub,.add{
+           width: 1.5em;
+           height: 1.5em;
+           text-align: center;
+           display: inline-block;
+        }
+        .value{
+            min-width: 1.5em;
+            height: 1.5em;
+            border: 1rpx solid;
+            text-align: center;
+            border-color: transparent #eee transparent #eee;
+        }
+    }
+</style>
+<template>
+  <view class="counter-component">
+      <view class="sub" bindtap="sub">-</view>
+      <view class="value">{{value}}</view>
+      <view class="add" bindtap="add">+</view>
+  </view>
+</template>
+<config>
+{
+    "component": true
+}
+</config>
+<script>
+
+    Component({
+        properties:{
+            min:{
+                type:Number,
+                value:0
+            },
+            max:{
+                type:Number,
+                value:Infinity
+            },
+            value:{
+                type:Number,
+                value:0
+            }
+        },
+        methods:{
+            add(){  
+                let {value,min,max} = this.data;
+                max = max||Infinity;
+                value = value + 1;
+                value = value > max ? max : value;
+                this.triggerEvent("add");
+                this.triggerEvent("change",value);
+            },
+            sub(){
+                let {value,min,max} = this.data;
+                value = value - 1;
+                value = value < min ? min : value;
+                this.triggerEvent("sub");
+                this.triggerEvent("change",value);
+            },
+            
+        }
+    })
+
+ 
+</script>
